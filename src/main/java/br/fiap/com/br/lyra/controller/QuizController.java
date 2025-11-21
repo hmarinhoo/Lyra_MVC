c9e74b285d37aecfa90c0e4f98befc5fb564ba30
hmarinhoo <hellenmcordeiro@gmail.com>
2025-11-21 01:07:26 -0300

    ..

--- src/main/java/br/fiap/com/br/lyra/controller/QuizController.java
+++ src/main/java/br/fiap/com/br/lyra/controller/QuizController.java
@@ -6,9 +6,7 @@ import br.fiap.com.br.lyra.repository.QuizRepository;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 @Controller
 public class QuizController {
